7e1e83874396db5db6fbb257965c989c753c4381
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-02-20 10:18:37 -0600

    Verify landscape gradients

--- docs/R/landscapes.R
+++ docs/R/landscapes.R
@@ -1,37 +1,36 @@
 library(tidyverse)
 library(lattice)
+library(gems)
 
-# SimpleHill
-SimpleHill <- read_csv("experiment/gems/landscapes/SimpleHill.csv")
+data("SimpleHill")
+data("OrientationBias")
+data("SpatialFrequencyBias")
+data("ReverseOrientation")
+data("ReverseSpatialFrequency")
+data("ReverseBoth")
+
+# Compare SimpleHill and training landscapes
 wireframe(score ~ x * y, data = SimpleHill, xlab = "orientation", ylab = "spatial frequency ")
+wireframe(score ~ x * y, data = OrientationBias, xlab = "orientation", ylab = "spatial frequency ")
+wireframe(score ~ x * y, data = SpatialFrequencyBias, xlab = "orientation", ylab = "spatial frequency ")
 
-# Recreate SimpleHill
-grid <- expand.grid(
-  x = 0:99,
-  y = 0:99
-)
+# Test landscapes
+ori_plot <- ggplot() +
+  aes(x, ori) +
+  geom_point()
 
-simple_hill_func <- function(x, y) {
-  (-x**2) - (y**2) + (100*x) + (100*y)
-}
+sf_plot <- ggplot() +
+  aes(y, sf) +
+  geom_point()
 
-SimpleHill2 <- grid %>%
-  mutate(score = simple_hill_func(x, y))
+ori_plot %+% SimpleHill
+ori_plot %+% OrientationBias
+ori_plot %+% SpatialFrequencyBias
+ori_plot %+% ReverseOrientation
+ori_plot %+% ReverseBoth
 
-wireframe(score ~ x * y, data = SimpleHill2, xlab = "orientation", ylab = "spatial frequency ")
-
-# Biased orientation
-biased_orientation_func <- function(x, y) {
-  (-x**2) + (100*x)
-}
-BiasedOrientation <- grid %>%
-  mutate(score = biased_orientation_func(x, y))
-wireframe(score ~ x * y, data = BiasedOrientation, xlab = "orientation", ylab = "spatial frequency ")
-
-# Biased spatial frequency
-biased_spatial_frequency_func <- function(x, y) {
-  (-y**2) + (100*y)
-}
-BiasedSpatialFrequency <- grid %>%
-  mutate(score = biased_spatial_frequency_func(x, y))
-wireframe(score ~ x * y, data = BiasedSpatialFrequency, xlab = "orientation", ylab = "spatial frequency ")
+sf_plot %+% SimpleHill
+sf_plot %+% SpatialFrequencyBias
+sf_plot %+% OrientationBias
+sf_plot %+% ReverseSpatialFrequency
+sf_plot %+% ReverseBoth
